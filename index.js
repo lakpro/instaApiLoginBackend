3741b8d0252ea9277188018bd02125408aa8aa63
Lakshay Setia <lakshaysetia123@gmail.com>
2025-04-10 23:14:22 +0530

    updated cors

--- index.js
+++ index.js
@@ -13,10 +13,24 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const PORT = process.env.PORT || 5000;
-const cors = require("cors");
-app.use(cookieParser());
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://insta-api-login.netlify.app",
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
 
 // app.use(cookieParser());
 app.use("/api/auth", authRoutes);
